c80dde35ffe51f5c7816b97ce1d1caf77ed06331
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-11-15 20:28:17 +0700

    Penambahan firebase google service dan penmabahan layout signin

---
+++ app/src/main/java/com/android/dtrescatering/SigninActivity.java
@@ -0,0 +1,20 @@
+package com.android.dtrescatering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        mHideActionBar();
+    }
+
+    private void mHideActionBar() {
+        getSupportActionBar().hide();
+    }
+}
