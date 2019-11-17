65e53ead6ed0d1952a2f06c12f88a29a97e1308f
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-11-17 20:25:51 +0700

    booking done, validate and metode pembayaran

--- app/src/main/java/com/android/dtrescatering/BookingActivity.java
+++ app/src/main/java/com/android/dtrescatering/BookingActivity.java
@@ -1,23 +1,34 @@
 package com.android.dtrescatering;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,76 +38,163 @@ import static com.android.dtrescatering.base.MethodeFunction.shortToast;
 
 public class BookingActivity extends AppCompatActivity {
 
-    TextView lokasi;
+    private EditText mAlamatEditText;
+    private Button mTambahButton;
+    private Button mKurangButton;
+    private Button mOrderButton;
+    private TextView mNamaTextView;
+    private TextView mHargaTextView;
+    private TextView mJumlahTextView;
+    private TextView mTotalTextView;
+    private TextView mOngkirTextView;
+    private TextView mSubTotalTextView;
+    private int qty = 0;
+
+    private String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    private String username = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
-        lokasi = findViewById(R.id.textView_tes_lokasi);
+        mAlamatEditText = findViewById(R.id.editText_booking_address);
+        mTambahButton = findViewById(R.id.button_booking_plus);
+        mKurangButton = findViewById(R.id.button_booking_min);
+        mOrderButton = findViewById(R.id.button_booking_order);
+        mNamaTextView = findViewById(R.id.textView_booking_nama);
+        mHargaTextView = findViewById(R.id.textView_booking_harga);
+        mJumlahTextView = findViewById(R.id.textView_booking_qty);
+        mTotalTextView = findViewById(R.id.textView_booking_total_price);
+        mOngkirTextView = findViewById(R.id.textView_booking_ongkir);
+        mSubTotalTextView = findViewById(R.id.textView_booking_sub_total);
 
+        mShowData(savedInstanceState);
+    }
 
-        lokasi.setOnClickListener(new View.OnClickListener() {
+    private void mShowData(final Bundle savedInstanceState) {
+        String itemId = getItemId(savedInstanceState);
+        String storeId = getStoreId(savedInstanceState);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Stores").child(storeId).child("items").child(itemId);
+
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                orderButton();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String harga = dataSnapshot.child("harga").getValue(String.class);
+                String nama = dataSnapshot.child("nama").getValue(String.class);
+
+                mNamaTextView.setText(nama);
+                mHargaTextView.setText(harga);
+
+                mButtonQtyClicked(harga, savedInstanceState);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
 
-    private void mGetAddress() {
-        FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+    private void mButtonQtyClicked(final String harga, final Bundle bundle) {
+        mTambahButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(Location location) {
-                if (location != null){
-//                    rawLocation = location.getLatitude();
-////                    // Do it all with location
-////                    Log.d("My Current location", "Lat : " + location.getLatitude() + " Long : " + location.getLongitude());
-////                    // Display in Toast
-////                    Toast.makeText(MainActivity.this,
-////                            "Lat : " + location.getLatitude() + " Long : " + location.getLongitude(),
-////                            Toast.LENGTH_LONG).show();
-                    Geocoder geocoder;
-                    List<Address> addresses;
-                    geocoder = new Geocoder(BookingActivity.this, Locale.getDefault());
+            public void onClick(View view) {
+                qty = qty + 1;
+                int hargaitem = Integer.parseInt(harga) * qty;
+                int ongkir = Integer.parseInt(mOngkirTextView.getText().toString());
+                int subTotal = hargaitem + ongkir;
 
-                    try {
-                        addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                mJumlahTextView.setText(String.valueOf(qty));
+                mTotalTextView.setText(String.valueOf(hargaitem));
+                mSubTotalTextView.setText(String.valueOf(subTotal));
+            }
+        });
 
-                        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-                        String city = addresses.get(0).getLocality();
-                        String state = addresses.get(0).getAdminArea();
-                        String country = addresses.get(0).getCountryName();
-                        String postalCode = addresses.get(0).getPostalCode();
-                        String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+        mKurangButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (qty <= 0) {
+                    shortToast(getApplicationContext(), "Tidak Bisa dibawah 0");
+                } else {
+                    qty = qty - 1;
+                    int hargaitem = Integer.parseInt(mTotalTextView.getText().toString()) - Integer.parseInt(harga);
+                    int ongkir = Integer.parseInt(mOngkirTextView.getText().toString());
+                    int subTotal = hargaitem - ongkir;
 
+                    mJumlahTextView.setText(String.valueOf(qty));
+                    mTotalTextView.setText(String.valueOf(hargaitem));
 
-
-                        lokasi.setText(address + "n" + city + "n" + state + "n" + country + "n" + postalCode + "n" + knownName);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    if (subTotal < 0) {
+                        mSubTotalTextView.setText("0");
+                    } else {
+                        mSubTotalTextView.setText(String.valueOf(subTotal));
                     }
+
                 }
             }
         });
 
-
-
-//        addresses = geocoder.getFromLocation()
+        mOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage(bundle);
+            }
+        });
     }
 
-    private void orderButton() {
-        if (ActivityCompat.checkSelfPermission(BookingActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(BookingActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(BookingActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            shortToast(this, "Tekan lagi untuk melanjutkan");
-            return;
+    private String getStoreId(Bundle savedInstanceState) {
+        String storeId;
+        if (savedInstanceState == null) {
+            Bundle bundle = getIntent().getExtras();
+            if (bundle == null) {
+                return storeId = null;
+            } else {
+                return storeId = bundle.getString("storeId");
+            }
         } else {
-            // Write you code here if permission already given.
-            mGetAddress();
+            return storeId = (String) savedInstanceState.getSerializable("storeId");
         }
     }
 
+    private String getItemId(Bundle savedInstanceState) {
+        String itemId;
+        if (savedInstanceState == null) {
+            Bundle bundle = getIntent().getExtras();
+            if (bundle == null) {
+                return itemId = null;
+            } else {
+                return itemId = bundle.getString("itemId");
+            }
+        } else {
+            return itemId = (String) savedInstanceState.getSerializable("itemId");
+        }
+    }
 
+    private void sendMessage(Bundle bundle) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(getStoreId(bundle));
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String phone = dataSnapshot.child("phone").getValue(String.class);
+                Uri uri = Uri.parse("smsto:" + phone);
+
+                String alamat = mAlamatEditText.getText().toString();
+                String namaBarang = mNamaTextView.getText().toString();
+                String total = String.valueOf(qty);
+
+                String message = "Saya pesan " + namaBarang + " sebanyak " + total + " porsi. dikirim ke : " + alamat;
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra("sms_body",message);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
